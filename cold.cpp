9f5e22c9439ae302ffdd7a901bb792d2f64927d2
Connor Ford <connorford23@gmail.com>
2020-01-03 18:27:59 -0600

    solution for cold-puter science

---
+++ cold.cpp
@@ -0,0 +1,20 @@
+// URL - https://open.kattis.com/problems/cold
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int count = 0;
+    int temps, k, num;
+    cin >> temps;
+
+    for (k = 0; k < temps; k++) {
+        cin >> num;
+        if (num < 0)
+            count++;
+    }
+
+    cout << count << endl;
+
+    return 0;
+}
