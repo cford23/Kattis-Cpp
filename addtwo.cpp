6eef47d9c18172b7aa9801d7451e0450f361b36f
clford <connorford23@gmail.com>
2021-07-03 09:38:49 -0500

    solution for add two numbers

---
+++ addtwo.cpp
@@ -0,0 +1,11 @@
+// URL - https://open.kattis.com/problems/addtwonumbers
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << (a + b) << endl;
+
+    return 0;
+}
