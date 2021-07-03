60d4b4b98e8d99cbbc4d43e2e2d20d21dc3e31ca
clford <connorford23@gmail.com>
2021-07-03 09:27:55 -0500

    solution for echo echo echo

---
+++ echo.cpp
@@ -0,0 +1,15 @@
+// URL - https://open.kattis.com/problems/echoechoecho
+#include <iostream>
+using namespace std;
+
+int main() {
+    string word;
+    int k;
+
+    cin >> word;
+    for (k = 0; k < 3; k++)
+        cout << word << " ";
+    cout << endl;
+
+    return 0;
+}
